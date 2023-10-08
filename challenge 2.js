d20eddb004aa0717ff34f300dec436339f1e8165
ngendor00t <145445244+ngendor00t@users.noreply.github.com>
2023-10-08 22:20:19 +0300

    Update challenge 2.js

--- challenge 2.js
+++ challenge 2.js
@@ -1,1 +1,10 @@
-let speed = speed < 70 ? "ok": "5km> 70 print demeritpoints"
+function speedDectector (input){
+  if (input<70)
+    return "ok";
+}
+else if (input>70){
+  extra/5
+  where 5 stands for 1 point
+  return"demirit points" ;
+}
+speedDetector(70)

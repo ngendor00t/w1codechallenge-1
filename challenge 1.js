747832bb4da664c49ea884378d5620f5688cf330
ngendor00t <145445244+ngendor00t@users.noreply.github.com>
2023-10-08 21:46:52 +0300

    Update challenge 1.js

--- challenge 1.js
+++ challenge 1.js
@@ -4,16 +4,16 @@ if (input>79){
  return 'A' ;
 }
 
-else if (input60-79)
+else if (input<=79 7&& input>=60){
 return 'B-';
-
-else if (input59-49){
+}
+else if (input<=59 && input>=49){
     return 'C-' ;
 }
-else if (input40-49){
-    return 'D-'
+else if (input<=49 && input>=40){
+    return 'D-' ;
 }
  
 else if (input<40){
-    return 'E-'
+    return 'E-' ;
 }

85fa4cb116972b9ced0d0c113442de14b18d37b6
ngendor00t <145445244+ngendor00t@users.noreply.github.com>
2023-10-13 10:53:22 +0300

    Update challenge 1.js
    resubmission

--- challenge 1.js
+++ challenge 1.js
@@ -1,15 +1,16 @@
 let studentMarks =(input0-100)
-
+//Here I was trying to create a program for grades
 if (input>79){
  return 'A' ;
 }
-
+//if the student managed to get a input above 79 the student got a grade A.
 else if (input<=79 7&& input>=60){
 return 'B-';
 }
 else if (input<=59 && input>=49){
     return 'C-' ;
 }
+ //other wise if a student got an input between 49-59 they get the grade (c-)
 else if (input<=49 && input>=40){
     return 'D-' ;
 }
@@ -17,3 +18,4 @@ else if (input<=49 && input>=40){
 else if (input<40){
     return 'E-' ;
 }
+console.log(studentMarks)

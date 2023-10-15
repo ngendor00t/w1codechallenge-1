b288614522ba53c42e13c528471b316ad4c70f24
ngendor00t <145445244+ngendor00t@users.noreply.github.com>
2023-10-15 14:24:31 +0300

    Update challenge 1.js

--- challenge 1.js
+++ challenge 1.js
@@ -1,21 +1,27 @@
-let studentMarks =(input0-100)
+
+function studentMarks(){
+
+let input = prompt()
 //Here I was trying to create a program for grades
-if (input>79){
- return 'A' ;
+    if (input>79){
+     return 'A' ;
+    }
+    //if the student managed to get an input above 79 the student got a grade A.
+    else if (input<=79 && input>=60){
+    return 'B-';
+    }
+    else if (input<=59 && input>=49){
+        return 'C-' ;
+    }
+     //otherwise if a student got an input between 49-59 they get the grade (c-)
+    else if (input<=49 && input>=40){
+        return 'D-' ;
+    }
+     
+    else if (input<40){
+        return 'E-' ;
+    }
 }
-//if the student managed to get a input above 79 the student got a grade A.
-else if (input<=79 7&& input>=60){
-return 'B-';
-}
-else if (input<=59 && input>=49){
-    return 'C-' ;
-}
- //other wise if a student got an input between 49-59 they get the grade (c-)
-else if (input<=49 && input>=40){
-    return 'D-' ;
-}
- 
-else if (input<40){
-    return 'E-' ;
-}
-console.log(studentMarks)
+
+
+console.log(studentMarks())

048d73bcf1ee8c062ff84b74f550534648628aaa
ngendor00t <145445244+ngendor00t@users.noreply.github.com>
2023-10-22 12:28:01 +0300

    Update challenge 1.js

--- challenge 1.js
+++ challenge 1.js
@@ -1,27 +1,26 @@
+//creating a function to carry on with the program
+function generateStudentGrades(mark){
 
-function studentMarks(){
-
-let input = prompt()
-//Here I was trying to create a program for grades
-    if (input>79){
-     return 'A' ;
-    }
-    //if the student managed to get an input above 79 the student got a grade A.
-    else if (input<=79 && input>=60){
-    return 'B-';
-    }
-    else if (input<=59 && input>=49){
-        return 'C-' ;
-    }
-     //otherwise if a student got an input between 49-59 they get the grade (c-)
-    else if (input<=49 && input>=40){
-        return 'D-' ;
-    }
-     
-    else if (input<40){
-        return 'E-' ;
-    }
+if(mark > 79){
+    grade = 'A';
 }
+    //calculating students' grade that is equivalent to the grade they got.
+else if(mark >= 60 && mark <= 79){
+    grade = 'B';
+}else if(mark >= 50 && mark <= 59){
+    grade = 'C';
+}
+// putting conditional  statements if at all they didn't get a specific mark that registers a grade then they get a different grade
+else if(mark >= 40 && mark <= 49){
+    grade = 'D';
+}else{
+    grade = 'E';
+}
+console.log(`The Student's grade is: ${grade}`)
 
 
-console.log(studentMarks())
+
+}
+//invoking the function so that the function can work
+
+generateStudentGrades(7)

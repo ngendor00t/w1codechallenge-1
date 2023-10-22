a1a1d412155edc7f37345ce77bfde95d40440d8a
ngendor00t <145445244+ngendor00t@users.noreply.github.com>
2023-10-22 20:41:57 +0300

    Update challenge 3.js

--- challenge 3.js
+++ challenge 3.js
@@ -1,18 +1,38 @@
-let netSalary = basicSalary-taxes {
-    
-basic salary (input)
-benefits (inputs)
+          
 
-    function paye(input grossSalary){
-        if grossSalary (0>=24,000)
-        return 10 %
+function calculateNetSalary(basicSalary, benefits) {
 
-    else if (input>=24,001 && input <=32,333)
-            return 25.0%
+    const taxRate = 0.2
+    const nhifRate = 0.1
+    const nssfRate = 0.2
 
 
+    const grossSalary = basicSalary + benefits;
 
-    else if (input>= 32,334 && input <= 5000,000)
-                return 30.0 %
-                
 
+    const tax = grossSalary * taxRate;
+    const nhif = grossSalary * nhifRate;
+    const nssf = grossSalary * nssfRate;
+
+
+    const netSalary = grossSalary - (tax + nhif + nssf);
+
+    return {
+      grossSalary,
+      tax,
+      nhif,
+      nssf,
+      netSalary
+    };
+  }
+
+
+  const basicSalary = 59000; 
+  const benefits =4500; 
+  const result = calculateNetSalary(basicSalary, benefits);
+  console.log("Net Salary Calculation Results:");
+  console.log("Gross Salary:", result.grossSalary);
+  console.log("Tax:", result.tax);
+  console.log("NHIF Deductions:", result.nhif);
+  console.log("NSSF Deductions:", result.nssf);
+  console.log("Net Salary:", result.netSalary);
